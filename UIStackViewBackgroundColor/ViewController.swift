2f3b5f3197def20d25efd4f9c700a91c567b30b4
user140592 <user140592@eu201.local>
2019-04-07 20:01:07 +0100

    Initial Commit

---
+++ UIStackViewBackgroundColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIStackViewBackgroundColor
+//
+//  Created by user140592 on 4/7/19.
+//  Copyright © 2019 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
